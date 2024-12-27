37ac8806601f6b9c842917e50614b71972d19b3c
barsi12 <82933338+barsi12@users.noreply.github.com>
2024-12-27 05:23:35 -0800

    REALESTATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-//import 'font-awesome/css/font-awesome.min.css';
+//importttt 'font-awesome/css/font-awesome.min.css';
 import Header from "./components /Header/Header";
 import Hero from "./components /Hero/Hero";
 
